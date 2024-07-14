d7cd7119e6984e76485814fd1300cc76eb6ce4d0
Abidh <aabidhrashyd@gmail.com>
2024-07-14 21:42:57 +0500

    14th july
    pages adding upto detail

--- store/apps/urls.py
+++ store/apps/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.apps, name='apps'),
+    path('contact', views.contact , name='contact'),
+    path('detail', views.detail , name='detail')
 ]
--- store/apps/views.py
+++ store/apps/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 def apps(request):
     #return HttpResponse("Hello world!")
     return render(request, 'index.html')
+
+def contact(request):
+    #return HttpResponse("Hello world!")
+    return render(request, 'contact.html')
+
+def detail(request):
+    #return HttpResponse("Hello world!")
+    return render(request, 'detail.html')
